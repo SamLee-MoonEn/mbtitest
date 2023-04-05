84cbeea127722e66a16d48bde08cbf4d90fae279
SamLee <goha2010@naver.com>
2023-04-05 22:56:18 +0900

    Naver 사이트 소유확인 코드 추가 및 Tailwindcss/daisyUi 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 
 import MainPage from './pages/MainPage'
 import QuestionPage from './pages/QuestionPage'
